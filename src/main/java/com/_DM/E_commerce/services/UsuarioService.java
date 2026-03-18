5f096ece5408533aeec60ccd1ecca4415cdfaa53
GoBrazill <vinicius.daraujos26@gmail.com>
2026-03-18 11:09:20 +0000

    feat: show and update user methods

--- src/main/java/com/_DM/E_commerce/services/UsuarioService.java
+++ src/main/java/com/_DM/E_commerce/services/UsuarioService.java
@@ -3,7 +3,13 @@ package com._DM.E_commerce.services;
 import com._DM.E_commerce.dtos.UsuarioDTO;
 import com._DM.E_commerce.entities.Usuario;
 import com._DM.E_commerce.repositories.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UsuarioService {
@@ -19,5 +25,35 @@ public class UsuarioService {
         usuarioRepository.save(usuario);
         UsuarioDTO usuarioDTO = new UsuarioDTO(usuario.getNome(), usuario.getEmail(), usuario.getTelefone(), usuario.getSenha(), usuario.getRole());
         return usuarioDTO;
+    }
+
+    public List<Usuario> mostrarUsuarios() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        List<Usuario> listaUsuarios = new ArrayList<>();
+
+        listaUsuarios = usuarios;
+
+        return listaUsuarios;
+    }
+
+    public UsuarioDTO mostrarUsuarioPorId(UUID id) {
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com id: " + id));
+
+        return new UsuarioDTO(usuario.getNome(), usuario.getEmail(), usuario.getTelefone(), usuario.getSenha(), usuario.getRole());
+    }
+
+    public String atualizarUsuario(UUID id, UsuarioDTO dto) {
+        Optional<Usuario> listaUsuarios = usuarioRepository.findById(id);
+
+        if (listaUsuarios.isPresent()) {
+            Usuario usuario = listaUsuarios.get();
+            usuario.setNome(dto.getNome());
+            usuario.setEmail(dto.getEmail());
+
+            return usuario.toString();
+        } else {
+            return "Usuario não encontrado";
+        }
+
     }
 }
