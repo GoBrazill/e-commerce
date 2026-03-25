718fb1c4998e42fd1b77cb01c90057a1f31d4efb
GoBrazill <vinicius.daraujos26@gmail.com>
2026-03-24 23:29:42 -0300

    feat: add pedido service crud methods

--- src/main/java/com/_DM/E_commerce/services/PedidoService.java
+++ src/main/java/com/_DM/E_commerce/services/PedidoService.java
@@ -1,9 +1,14 @@
 package com._DM.E_commerce.services;
 
-import com._DM.E_commerce.dtos.PedidoDTO;
 import com._DM.E_commerce.entities.Pedido;
+import com._DM.E_commerce.dtos.PedidoDTO;
 import com._DM.E_commerce.repositories.PedidoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PedidoService {
@@ -17,8 +22,45 @@ public class PedidoService {
     public PedidoDTO criarPedido(PedidoDTO dto) {
             Pedido pedido = new Pedido(dto.getMomento(), dto.getStatus());
             pedidoRepository.save(pedido);
-            PedidoDTO pedidodto = new PedidoDTO(pedido.getMomento(), pedido.getStatus());
-            return pedidodto;
+
+            return new PedidoDTO(pedido.getMomento(), pedido.getStatus());
+    }
+
+    public List<Pedido> mostrarPedidos() {
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        List<Pedido> listaPedidos;
+
+        listaPedidos = pedidos;
+
+        return listaPedidos;
+    }
+
+    public PedidoDTO mostrarPedidoPorId(UUID id) {
+        Pedido pedido = pedidoRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com id: " + id));
+
+        return new PedidoDTO(pedido.getMomento(), pedido.getStatus());
+    }
+
+    public String atualizarPedido(UUID id, PedidoDTO dto) {
+        Optional<Pedido> listaPedidos = pedidoRepository.findById(id);
+
+        if (listaPedidos.isPresent()) {
+            Pedido pedido = listaPedidos.get();
+            pedido.setStatus(dto.getStatus());
+
+            return pedido.toString();
+        } else {
+            return "Pedido não encontrado";
+        }
+    }
+
+    public String deletarPedido(UUID id) {
+        if (pedidoRepository.existsById(id)) {
+            pedidoRepository.deleteById(id);
+            return "Pedido excluído com sucesso";
+        } else {
+            return "Pedido não encontrado";
+        }
     }
 
 }
